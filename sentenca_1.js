04612ab5e04c90156dce4213398f71e206a15d90
João Lacerda <joao.sclf@gmail.com>
2022-12-20 23:12:49 -0300

    vareiaveis-js

--- sentenca_1.js
+++ sentenca_1.js
@@ -1,3 +1,13 @@
-console.log('Passo #01');
-console.log('Passo #02');
-console.log('Passo #03');
+var nome = "Caneta"
+var quantidade = 10;
+var preco = 6.4;
+var imposto = 1.5;
+var precoFinal = preco + imposto;
+
+console.log(nome);
+console.log(quantidade);
+console.log(preco);
+console.log(imposto);
+console.log(precoFinal);
+
+console.log(nome);
