cd76c52d46abb0eea5538ba1eab98d193a3074d1
João Lacerda <joao.sclf@gmail.com>
2022-12-23 00:08:32 -0300

    tipo-de-dados

---
+++ dados_3.js
@@ -0,0 +1,11 @@
+let idade = 26;
+console.log(typeof 26);
+console.log(typeof idade);
+
+let salario = 4785.38;
+console.log(typeof salario)
+
+let estaChovendo = true;
+console.log(typeof estaChovendo);
+
+console.log(typeof "teste");
